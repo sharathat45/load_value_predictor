99f2771e3e573fa06a1ca0dc800d6a24392c2a92
Om G <im.omgupta@gmail.com>
2023-12-14 19:50:46 -0500

    Add new charts

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ SRC_H = include
 ASSEMBLY_FLAG = -S
 CC = /usr/bin/g++ -O2 -std=gnu++11 -I $(SRC_H) -L util/m5/build/x86/out/
 
-MODEL = ./build/ECE565-ARM/gem5.debug 
+MODEL = ./build/ECE565-ARM/gem5.opt 
 CONFIG = ./configs/example/se.py 
 CONFIG_FLAGS = --cpu-type=O3CPU --maxinsts=1000000 --l1d_size=64kB --l1i_size=16kB --caches --l2cache
 HELLO_EXECUTABLE = tests/test-progs/hello/bin/arm/linux/hello
@@ -35,8 +35,7 @@ $(EXECUTABLE): $(SRC_C)
 	$(CC) $(SRC_C) -o $(EXECUTABLE) -lm5 
 
 build:
-	scons USE_HDF5=0 -j `nproc` $(MODEL)
-#  build/x86/out/m5
+	scons USE_HDF5=0 -j `nproc` --linker=gold $(MODEL)
 
 run: $(EXECUTABLE)
 	$(MODEL) $(DEBUG_FLAGS) $(CONFIG) $(CONFIG_FLAGS) -c $(EXECUTABLE)
--- analysis/collect_lvp_stats.py
+++ analysis/collect_lvp_stats.py
@@ -5,7 +5,7 @@ import time
 from typing import List, Dict, Union
 import sys
 
-import latextable
+import pandas as pd
 
 from m5stats import M5Stats
 
@@ -21,6 +21,17 @@ LVPSTATS_PATH = "system.cpu.lvp"
 BENCHMARK_NAMES = ("sjeng", "leslie3d", "lbm", "astar", "milc", "namd")
 
 STATSFILE_SUFFIX = "_stats.txt"
+
+C_LOOKUP = 'lookups'
+C_PRED_TOTAL = 'predTotal'
+C_PRED_CORRECT = 'predCorrect'
+C_PRED_INCORRECT = 'predIncorrect'
+C_PRED_RATE = 'predRate'
+C_PRED_ACC = 'predAccuracy'
+C_CONST_PRED = 'constPred'
+C_CONST_INV = 'constInval'
+C_CONST_RB = 'constRollback'
+C_CONST_RBRATE = 'constRollbackRate'
 
 
 def run_benchmark(benchmark_name: str):
@@ -53,29 +64,71 @@ def lvp_chart(benchmark_name: str, out_filepath: str):
     )
 
 
-def lvp_table(benchmark_names: str, out_filepath: str):
-    table = latextable.texttable.Texttable(400)
+def read_lvp_stats(benchmark_names: List[str]) -> pd.DataFrame:
+    lvp_stats = {}
 
-    added_header = False
     for benchmark_name in benchmark_names:
         benchmark_path = STATS_DIRPATH / (benchmark_name + STATSFILE_SUFFIX)
-
-        stats = M5Stats(benchmark_path)
-        lvp_stats = stats.get_stats(LVPSTATS_PATH).as_dict()
-
-        if not added_header:
-            table.add_row(["Benchmark"] + [s for s in lvp_stats.keys()])
-            added_header = True
-        
-        table.add_row([benchmark_name] + [v for v in lvp_stats.values()])
+        lvp_stats[benchmark_name] = M5Stats(benchmark_path).get_stats(LVPSTATS_PATH).as_dict()
     
-    rendered = table.draw() + "\n\n\n" + latextable.draw_latex(
-        table = table,
-        caption = "LVP statistics for SPEC benchmarks",
-    )
+    return pd.DataFrame.from_dict(lvp_stats, orient='index')
 
-    with open(out_filepath, "w") as f:
-        f.write(rendered)
+
+def pred_rate_stats_table(lvp_stats: pd.DataFrame, out_filepath: str = None) -> pd.DataFrame:
+    pred_rate_stats = lvp_stats[[C_LOOKUP, C_PRED_TOTAL]].rename({
+        C_LOOKUP: 'LVP lookups',
+        C_PRED_TOTAL: 'LVP predictions',
+    })
+
+    pred_rate_stats['LVP prediction rate'] = lvp_stats[C_PRED_TOTAL]/lvp_stats[C_LOOKUP]
+    pred_rate_stats['LVP prediction rate'] = pred_rate_stats['LVP prediction rate'].map(lambda x: f"{100*x:.2f}%")
+
+    if out_filepath is not None:
+        with open(out_filepath, 'w') as f:
+            f.write(str(pred_rate_stats))
+            f.write("\n\n" + "*" * 80 + "\n\n")
+            pred_rate_stats.to_latex(f, caption="LVP Prediction Rate")
+    
+    return pred_rate_stats
+
+
+def pred_acc_stats_table(lvp_stats: pd.DataFrame, out_filepath: str = None) -> pd.DataFrame:
+    pred_acc_stats = pd.DataFrame()
+
+    pred_acc_stats['Dynamic predictions'] = lvp_stats[C_PRED_TOTAL]
+    pred_acc_stats['Committed predictions'] = lvp_stats[C_PRED_CORRECT] + lvp_stats[C_PRED_INCORRECT]
+
+    pred_acc_stats['Correct predictions'] = lvp_stats[C_PRED_CORRECT]
+    pred_acc_stats['Incorrect predictions'] = lvp_stats[C_PRED_INCORRECT]
+
+    pred_acc_stats['Prediction accuracy'] = lvp_stats[C_PRED_CORRECT]/pred_acc_stats['Committed predictions']
+    pred_acc_stats['Prediction accuracy'] = pred_acc_stats['Prediction accuracy'].map(lambda x: f"{100*x:.2f}%")
+
+    if out_filepath is not None:
+        with open(out_filepath, 'w') as f:
+            f.write(str(pred_acc_stats))
+            f.write("\n\n" + "*" * 80 + "\n\n")
+            pred_acc_stats.to_latex(f, caption="LVP Prediction Accuracy")
+    
+    return pred_acc_stats
+
+
+def pred_const_stats_table(lvp_stats: pd.DataFrame, out_filepath: str) -> pd.DataFrame:
+    pred_const_stats = pd.DataFrame()
+
+    pred_const_stats['Dynamic predictions'] = lvp_stats[C_PRED_TOTAL]
+    pred_const_stats['Constant predictions'] = lvp_stats[C_CONST_PRED]
+
+    pred_const_stats['Constant fraction'] = lvp_stats[C_CONST_PRED]/lvp_stats[C_PRED_TOTAL]
+    pred_const_stats['Constant fraction'] = pred_const_stats['Constant fraction'].map(lambda x: f"{100*x:.2f}%")
+
+    if out_filepath is not None:
+        with open(out_filepath, 'w') as f:
+            f.write(str(pred_const_stats))
+            f.write("\n\n" + "*" * 80 + "\n\n")
+            pred_const_stats.to_latex(f, caption="LVP Prediction Accuracy")
+    
+    return pred_const_stats
 
 
 def main():
@@ -94,9 +147,18 @@ def main():
 
         end = time.perf_counter()
         print(f"Done in {(end - start)*1000:.3f} ms")
-    
-    print("Generating table")
-    lvp_table(BENCHMARK_NAMES, OUTPUT_DIRPATH / f"lvp_table.txt")
+
+    print("Parsing LVP stats")
+    lvp_stats = read_lvp_stats(BENCHMARK_NAMES)
+
+    print("Generating prediction rate table")
+    pred_rate_stats_table(lvp_stats, OUTPUT_DIRPATH / "lvp_pred_rate.txt")
+
+    print("Generating prediction accuracy table")
+    pred_acc_stats_table(lvp_stats, OUTPUT_DIRPATH / "lvp_pred_acc.txt")
+
+    print("Generating constant predictions table")
+    pred_const_stats_table(lvp_stats, OUTPUT_DIRPATH / "lvp_pred_const.txt")
 
     program_end = time.perf_counter()
     print(f"Finished in {(program_end - program_start)*1000:.3f} ms")
